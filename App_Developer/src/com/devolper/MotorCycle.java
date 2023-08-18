93ce8af106777d784f85c45516da804d9e7064fb
upendra.bellamkonda <upendra.bellamkonda@HHCL-IT-12778.heterohcl.com>
2023-08-18 09:04:27 +0530

    Motor cycle class added

---
+++ App_Developer/src/com/devolper/MotorCycle.java
@@ -0,0 +1,19 @@
+package com.devolper;
+
+public class MotorCycle implements Vehicle {
+
+	@Override
+	public void Bike() {
+		// TODO Auto-generated method stub
+		System.out.println("2 wheeler Vehicle");
+
+	}
+
+	@Override
+	public void Car() {
+		// TODO Auto-generated method stub
+		System.out.println("4 wheeler vehicle");
+
+	}
+
+}
